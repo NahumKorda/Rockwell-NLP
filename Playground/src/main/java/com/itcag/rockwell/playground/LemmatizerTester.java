d09979a21a1d9e7f749f08103e8b854e37a0f002
Nahum <nahum.korda@it-consulting-ag.com>
2020-04-26 19:53:59 +0300

    LemmatizerTester class added to the Playground project.

--- Playground/src/main/java/com/itcag/rockwell/playground/LemmatizerTester.java
+++ Playground/src/main/java/com/itcag/rockwell/playground/LemmatizerTester.java
@@ -1,23 +1,27 @@
 package com.itcag.rockwell.playground;
 
 import com.itcag.rockwell.lang.Token;
+import com.itcag.rockwell.pipeline.Pipeline;
+import com.itcag.rockwell.pipeline.PropertyFields;
 import com.itcag.rockwell.tokenizer.Lemmatizer;
 import com.itcag.rockwell.tokenizer.Tokenizer;
 import com.itcag.rockwell.util.TokenPrinter;
 import com.itcag.util.Printer;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class LemmatizerTester {
 
     public static void main(String[] args) throws Exception {
 
-        String test = "raised $1.2 million in seed funding";
+        String test = "company raised $1.2 million in seed funding";
         
-        Tokenizer tokenizer = new Tokenizer();
-        ArrayList<String> stringTokens = tokenizer.getTokens(new StringBuilder(test));
+        Properties properties = new Properties();
+        properties.put(PropertyFields.TASK.getField(), Pipeline.Tasks.LEMMATIZE.name());
+
+        Pipeline pipeline = new Pipeline(properties);
         
-        Lemmatizer lemmatizer = new Lemmatizer();
-        ArrayList<Token> tokens = lemmatizer.getTokens(stringTokens);
+        ArrayList<Token> tokens = pipeline.lemmatize(pipeline.tokenize(new StringBuilder(test)));
 
         TokenPrinter.printTokensWithPOS(tokens);
         
