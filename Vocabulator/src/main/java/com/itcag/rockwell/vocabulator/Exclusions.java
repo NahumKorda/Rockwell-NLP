34ec98763675961c200308d10feca46fa6aa8102
Nahum <nahum.korda@it-consulting-ag.com>
2020-04-26 10:37:26 +0300

    License header trial.

--- Vocabulator/src/main/java/com/itcag/rockwell/vocabulator/Exclusions.java
+++ Vocabulator/src/main/java/com/itcag/rockwell/vocabulator/Exclusions.java
@@ -1,4 +1,23 @@
+/*
+ *
+ * Copyright 2020 IT Consulting AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.itcag.rockwell.vocabulator;
+
 
 /**
  * <p>Enumerates all possible exclusions that can be specified by the {@link com.itcag.rockwell.vocabulator.PropertyFields#EXCLUSIONS EXCLUSIONS} field.</p>
